12de5358dfba4825c8a37372be92b94a9dd10c3b
kayalvizhirajasekar <158528121+kayalvizhirajasekar@users.noreply.github.com>
2025-04-08 08:42:47 +0530

    Add files via upload

---
+++ day13/BaseClass.java
@@ -0,0 +1,35 @@
+package week4.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseClass {
+//global declaration
+	public ChromeDriver driver;
+
+	@BeforeMethod
+	public void preVonditions() {
+		driver = new ChromeDriver();
+		driver.get("https://login.salesforce.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+		//Username: dilip@testleaf.com
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("dilip@testleaf.com");
+		//Password: testleaf@2025
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Test@2025");
+       //Click Login buttom
+		driver.findElement(By.xpath("//input[@id='Login']")).click();
+	}
+	@AfterMethod
+	public void postConditions() {
+		driver.close();
+		
+		
+
+	}
+
+}
